451f8397d350d5aa2b413a885ce3ff11ab5ea33f
Federico Passarelli <federico2797@gmail.com>
2023-03-26 15:39:23 -0300

    Solution to my first frontend mentor challenge

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      hueRotate: {
+        '354': '354deg',
+        354: '354deg',
+      }
+    },
+  },
+  plugins: [],
+}
